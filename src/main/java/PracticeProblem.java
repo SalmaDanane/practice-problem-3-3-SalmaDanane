f05dfa72b26961cccf2d60d59ded3e80a46b2d31
SalmaDanane <343249595@tdsb.ca>
2026-05-05 07:56:35 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,59 @@
-public class PracticeProblem {
+public class PracticeProblem{
 
-	public static void main(String args[]) {
+    public static void main(String args[]){
+    }
 
-	}
+    public static String evenOrOdd(int num){
+ String bum;
+ if (num == 0){
+    bum = "Zero";
+ }
+ else if (num % 2 == 0) {
+    bum = "Even";
+ }
+ else {
+    bum = "Odd";
+ }
+ return bum;
+    }
 
-	
+public static String positiveOrNegative(int num) {
+    String pon;
 
+    if (num > 0 ){
+        pon = "Positive";
+    }
+    else if (num < 0){
+        pon = "Negative";
+
+    }
+    else {
+        pon = "Zero";
+    }
+    return pon;
+}
+
+
+public static String pluralize(String word) {
+
+    word = word.toLowerCase();
+    String bird = "something";
+
+    if(word.endsWith("ey")){
+        bird = "eys";
+    }
+    else if (word.endsWith("ife")){
+        bird = "ives";
+    }
+    else if (word.endsWith("y")){
+        bird = "ies";
+    }
+    else {
+        bird = "s";
+    }
+    return bird;
+
+
+}
+    
 }
