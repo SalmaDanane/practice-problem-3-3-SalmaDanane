935d20db3abc42b81331166c7179ea4ca93d67d7
SalmaDanane <343249595@tdsb.ca>
2026-06-03 10:43:33 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,50 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+   public static String evenOrOdd(int number) {
+	String evenOrOdd = "evenOrOdd";
+	if ((number % 2) == 0) {
+		evenOrOdd = "Even";
+	}
+	if (number == 0) {
+		evenOrOdd = "Zero";
+	}
+	else if ((number % 2) != 0) {
+		evenOrOdd = "Odd";
+	}
+	return evenOrOdd;
+   }
 
+   public static String positiveOrNegative(int number) {
+	String positiveOrNegative = "positiveOrNegative";
+	if (number > 0) {
+		positiveOrNegative = "Positive";
+	}
+	else if (number < 0) {
+		positiveOrNegative = "Negative";
+	}
+	else {
+		positiveOrNegative = "Zero";
+	}
+	return positiveOrNegative;
+   }
+   
+   public static String pluralize(String word) {
+	word = word.toLowerCase();
+	String pluralize = "pluralize";
+	if (word.endsWith("ey")) {
+		pluralize = "eys";
+	}
+	else if (word.endsWith("y")) {
+		pluralize = "ies";
+	}
+	else if(word.endsWith("ife")) {
+		pluralize = "ives";
+	}
+    else {
+		pluralize = "s";
+	}
+	return pluralize;
+   }
 	
-
-}
+	}
